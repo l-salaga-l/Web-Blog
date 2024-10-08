7a148780f91cf3c6165b9adf9e20af5945a43cce
R2okie <damir_ni@mail.ru>
2024-10-08 13:53:08 +0300

    Add blog details

--- src/main/java/com/example/webblog/controllers/BlogController.java
+++ src/main/java/com/example/webblog/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -36,5 +40,31 @@ public class BlogController {
                 .build();
         postRepository.save(post);
         return "redirect:/blog";
+    }
+
+    @GetMapping("/blog/{id}")
+    public String detailsPost(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-details";
+    }
+
+    @GetMapping("/blog/{id}/edit")
+    public String editPost(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> result = new ArrayList<>();
+        post.ifPresent(result::add);
+        model.addAttribute("post", result);
+        return "blog-edit";
     }
 }
